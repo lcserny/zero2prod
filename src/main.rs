0adac546b4984c1da25a86f03d14018c65e9546c
Leonardo Cserny <leonardo.cserny@endava.com>
2022-03-21 15:31:00 +0200

    impl proper db tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::{configurations::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to address.");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,9 @@
-use actix_web::{web::Form, HttpResponse};
+use actix_web::{
+    web::{self, Form},
+    HttpResponse,
+};
 use serde::Deserialize;
+use sqlx::PgPool;
 
 #[derive(Deserialize)]
 pub struct FormData {
@@ -7,6 +11,24 @@ pub struct FormData {
     pub name: String,
 }
 
-pub async fn subscribe(_form: Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(form: Form<FormData>, connection_pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        insert into subscriptions (id, email, name, subscribed_at) 
+        values ($1, $2, $3, $4)
+        "#,
+        uuid::Uuid::new_v4(),
+        form.email,
+        form.name,
+        chrono::Utc::now()
+    )
+    .execute(connection_pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
